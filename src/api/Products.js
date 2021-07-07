d0af61b6f73eaf27cb05a3846b074fd6722a9dff
kaiser-coder <miarintsoajohary@gmail.com>
2021-07-07 23:24:52 +0200

    [feat] Create products filtering by category

--- src/api/Products.js
+++ src/api/Products.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const fetch = () => {
   return axios
-    .get("https://fakestoreapi.com/products?limit=12")
+    .get("https://fakestoreapi.com/products?limit=24")
     .then((response) => {
       return response.data;
     });
--- src/views/Pages/Products.vue
+++ src/views/Pages/Products.vue
@@ -4,13 +4,14 @@
     <b-row>
       <b-col lg="6" class="ml-4">
         <b-nav>
-          <b-nav-item link-classes="text-dark" disbaled
-            >Toutes les catégories:
+          <b-nav-item link-classes="text-dark" @click.prevent="query = ''"
+            >Toutes les catégories
           </b-nav-item>
           <b-nav-item
             link-classes="text-dark"
             v-for="(category, index) in categories"
             :key="index"
+            @click.prevent="query = category"
           >
             {{ category }}
           </b-nav-item>
@@ -29,10 +30,10 @@
       </cart>
 
       <div class="row mt-3 mb-3">
-        <!-- Product Card -->
+        <!-- Products Cards -->
         <card
           :product="product"
-          v-for="product in products"
+          v-for="product in filteredProducts"
           :key="product.id"
           @addToPanel="addProduct(product)"
         ></card>
@@ -46,9 +47,15 @@ import Card from "@/components/products/Card";
 import { mapState, mapActions } from "vuex";
 import Cart from "@/components/products/Cart.vue";
 import CartItem from "@/components/products/CartItem.vue";
+import _ from "lodash";
 
 export default {
   name: "products",
+  data() {
+    return {
+      query: "",
+    };
+  },
   components: {
     Card,
     Cart,
@@ -56,6 +63,13 @@ export default {
   },
   computed: {
     ...mapState("Products", ["products", "categories"]),
+    filteredProducts() {
+      if (this.query == "") {
+        return this.products;
+      } else {
+        return _.filter(this.products, { category: this.query });
+      }
+    },
   },
   mounted() {
     this.fetchData();
